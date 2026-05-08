e5a13cc70f3e010664c335e64832e94937cbaaa5
Licky367 <licky367@gmail.com>
2026-05-08 13:13:43 +0300

    Create faultService.js

---
+++ services/faultService.js
@@ -0,0 +1,32 @@
+const FaultLog = require("../modules/FaultLog");
+
+exports.createFault = async(data)=>{
+
+    return await FaultLog.create(data);
+};
+
+exports.getClientFaults = async(clientId)=>{
+
+    return await FaultLog
+    .find({ clientId })
+    .sort({ createdAt:-1 });
+};
+
+exports.getPendingFaults = async()=>{
+
+    return await FaultLog.find({
+        resolved:false
+    });
+};
+
+exports.resolveFault = async(id)=>{
+
+    return await FaultLog.findByIdAndUpdate(
+        id,
+        {
+            resolved:true,
+            resolvedAt:new Date()
+        },
+        { new:true }
+    );
+};
