7a4c441b4e449b298e47e27df529d93c1fa57b69
Licky367 <licky367@gmail.com>
2026-05-08 12:18:04 +0300

    Create middleware/authMiddlware.js

---
+++ middleware/authMiddlware.js
@@ -0,0 +1,11 @@
+module.exports = (req,res,next)=>{
+
+    if(!req.session.user){
+
+        req.flash("error","Please login first");
+
+        return res.redirect("/login");
+    }
+
+    next();
+};
