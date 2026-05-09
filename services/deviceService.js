78937da88a1019134fdc891e71bbd6957c041038
Licky367 <licky367@gmail.com>
2026-05-09 11:03:47 +0300

    Create deviceService.js

---
+++ services/deviceService.js
@@ -0,0 +1,92 @@
+const DeviceStatus = require("../modules/DeviceStatus");
+const crypto = require("crypto");
+
+// Generate API key
+const generateApiKey = () =>{
+    return crypto.randomBytes(24).toString("hex");
+};
+
+
+// =========================
+// CREATE DEVICE
+// =========================
+
+exports.createDevice = async(data)=>{
+
+    const apiKey = generateApiKey();
+
+    const device = await DeviceStatus.create({
+
+        deviceId:data.deviceId,
+
+        name:data.name,
+
+        location:data.location,
+
+        apiKey
+
+    });
+
+    return device;
+};
+
+
+// =========================
+// GET ALL DEVICES
+// =========================
+
+exports.getAllDevices = async()=>{
+
+    return await DeviceStatus
+    .find()
+    .populate("clientId")
+    .sort({ createdAt:-1 });
+};
+
+
+// =========================
+// GET UNASSIGNED DEVICES
+// =========================
+
+exports.getUnassignedDevices = async()=>{
+
+    return await DeviceStatus.find({
+        assigned:false
+    });
+};
+
+
+// =========================
+// ASSIGN DEVICE TO CLIENT
+// =========================
+
+exports.assignDevice = async(
+    deviceId,
+    clientId
+)=>{
+
+    return await DeviceStatus.findOneAndUpdate(
+
+        { deviceId },
+
+        {
+            clientId,
+            assigned:true
+        },
+
+        { new:true }
+
+    );
+};
+
+
+// =========================
+// GET CLIENT DEVICE
+// =========================
+
+exports.getClientDevice = async(clientId)=>{
+
+    return await DeviceStatus.findOne({
+        clientId
+    });
+};
